47c505dea0d2a73e82d84defbf596e3cd5ac172d
Miguel Angel García Santiago <midato.mx@gmail.com>
2022-03-21 13:22:36 -0600

    [INIT] 1. Initial update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {CryptoService} from "./app/services/crypto.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ng-cipher-lib';
+
+  constructor(private cryptoService: CryptoService) {
+  }
+
+  ngOnInit(): void {
+
+    setTimeout(async () => {
+      let value = "NhWuUkvEcBMZS8XdZIm1nln5QoyecExFjk0i6dFV8ainBCfBYtiGPyh1bnL6rXe87WQF2/KKLw68nPaONyULcQLeO/GD1ELgkGF421AVSulQpctfG8Cv8GifcluLT2OcdQe6dx1LBhPL4i0deHuHejcL3gcuccsfLLsgdqlPmBgUM+M416uIDBdaSSiKcG0bYOn1yeSBryT9vu6cww+12A7CLT3wTeHzPwBlvm9C7r15E1zHuJi6uzN4J5j+qO+cgXqIVI+/qNbjbC2McAZSX8ufwDRYuZrnUeylpvuCsBrULKCzrJ0kSmsZriobIhj/nWEr1OI16bdsC7Z7NbYbONyMod4+lp/h44uVfaGk1Xwq7wh7lXyFXL4pz1jbWK8vZiqI1txgL6A2qTsOuPf8RKskO7Ytl59jLs8XJMZRGONWqXvKJ7gMsRaooi0chu5dJLFFimXboryFq/+ZY75Q12lTNiDJvq2R1mxrS8scuB/k0DMayDmJ0o/MApZfpDnmETcOjf84UiRGBfnf5jzlEosWkdIysm904eA1ZbjhWQ++LyA1r5cUXNOzyKcz8bbsyEeGfqBjc7NjK33k0pghlA==";
+      let aesK = "Do3VJxoVc9QBzMpk6/Vhh7xH0pqd+784Sva9BjNR6YY=";
+      const hmacK = "m0sfw6fhuU8vhvJoxZ0r6ZWFZmp26kRh97eihPJntfI=";
+      const dec = await this.cryptoService.decryptAes(value, aesK, hmacK);
+      // console.log(dec);
+
+      value = "pruebas_pld@yopmail.com";
+      aesK = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA45SIe9RDkgr7vCGPF4dY\nFrF8vZdJv44Puh7AXjaj2OQXUcm8eOFrar0w1rrEZMRB+pO1+QKGubERSgunw5YD\nYPOyG5t8tZepawxnQIXSQU7oEsqzTFShSjs5K6HoLafwwDxi6KD0pGWbOH8NvX3Q\nfjn4P8pHpiAn4ZOflbzG2BeJmzvlwrZRDHqGK/IXiDEsJn1AOob9QIkdTHfHgmU1\n95DjMXxfl4ZQDIV51qaIRHoWa2mXgxiO1GVD+t7/tNOmsQy2rJ3BaZi4p/eDkfX9\ndU1wsQTWn9ZHzmiIZaKu3vdFLg1wwVLlSsKxUuGLSLad2QE4wYtsBBD/OmhdNQi9\njQIDAQAB";
+      const enc = await this.cryptoService.encryptRSA(value, aesK);
+      console.log(enc);
+    });
+  }
+
 }
---
+++ src/app/app/services/crypto.service.ts
@@ -0,0 +1,117 @@
+import {Injectable} from '@angular/core';
+
+import * as crypto from 'crypto';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CryptoService {
+
+  constructor() {
+  }
+
+  async encryptKey(value, aes, hmac) {
+    try {
+      const aesK = aes;
+      const hmacK = hmac;
+      const rest = value;
+      return await this.encryptAes(rest, aesK, hmacK);
+    } catch (e) {
+      // console.log('encryptKey: error: ', e);
+      return '';
+    }
+  };
+
+  async decryptKey(value: string, aes: string, hmac: string) {
+    try {
+      const aesK = aes;
+      const hmacK = hmac;
+      const rest = value;
+      return await this.decryptAes(rest, aesK, hmacK);
+    } catch (e) {
+      // console.log('decryptKey: error: ', e);
+      return '';
+    }
+  }
+
+  async encryptRSA(value, privateKey) {
+    try {
+      const key = '-----BEGIN PUBLIC KEY-----\n' + privateKey + '\n-----END PUBLIC KEY-----';
+      let encryptBuff = crypto.publicEncrypt(
+        {
+          key,
+          padding: crypto.constants.RSA_PKCS1_PADDING,
+        },
+        Buffer.from(value)
+      );
+      return encryptBuff.toString('base64');
+    } catch (e) {
+      // console.log('encryptRSA: error: ', e);
+      return '';
+    }
+  };
+
+  async decryptRSA(value, privateKey) {
+    try {
+      const key = '-----BEGIN PRIVATE KEY-----\n' + privateKey + '\n-----END PRIVATE KEY-----';
+      const paddingValue = crypto.constants.RSA_PKCS1_PADDING;
+
+      // @ts-ignore
+      const msg = new Buffer.from(value, 'base64');
+      return crypto.privateDecrypt({key, padding: paddingValue}, msg).toString();
+    } catch (e) {
+      // console.log('decryptRSA: error: ', e);
+      return '';
+    }
+  };
+
+  async encryptAes(plainText, aesK, hmack) {
+    const valueToEncrypt = plainText.replace(/['"]+/g, '');
+
+    let aesKey = Buffer.from(aesK, 'utf8');
+    aesKey = Buffer.from(aesK, 'base64');
+
+    // let aesHmac = Buffer.from(hmack, 'utf8');
+    // // @ts-ignore
+    // aesHmac = Buffer.from(aesHmac, 'base64');
+    const iv = crypto.randomBytes(16);
+    const cipher = crypto.createCipheriv(await this.getAlgorithm(aesKey), aesKey, iv);
+
+    let cipherText = Buffer.concat([cipher.update(Buffer.from(valueToEncrypt, 'utf8')), cipher.final()]);
+    const ivCipherText = Buffer.concat([iv, cipherText]);
+    const hmac = crypto.createHmac('SHA256', Buffer.from(hmack, 'base64')).update(ivCipherText).digest();
+    const ivCipherTextHmac = Buffer.concat([ivCipherText, hmac]);
+
+    return ivCipherTextHmac.toString('base64');
+  }
+
+  async decryptAes(encryptedValue: string, aesK: string, hmacK: string) {
+    const ivCipherTextHmac = Buffer.from(encryptedValue, 'base64');
+    const aesKey = Buffer.from(aesK, 'base64');
+    const hmacKey = Buffer.from(hmacK, 'utf8');
+    const macLength = crypto.createHmac('sha256', hmacKey).digest().length;
+
+    const cipherTextLength = ivCipherTextHmac.length - macLength;
+    const iv = ivCipherTextHmac.slice(0, 16);
+    const cipherText = ivCipherTextHmac.slice(16, cipherTextLength);
+    const decipher = crypto.createDecipheriv(await this.getAlgorithm(aesKey), aesKey, iv);
+
+    let decrypted = decipher.update(cipherText);
+    // @ts-ignore
+    decrypted += decipher.final();
+
+    return decrypted.toString();
+  }
+
+  async getAlgorithm(keyBase64: any) {
+    const key = Buffer.from(keyBase64, 'base64');
+    switch (key.length) {
+      case 16:
+        return 'aes-128-cbc';
+      case 32:
+        return 'aes-256-cbc';
+    }
+    throw new Error('getAlgorithm: Invalid key length: ' + key.length);
+  }
+
+}
