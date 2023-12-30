f872fb087e92f17eae6c5a1f1fa36b32c46193c9
Miguel Angel García Santiago <midato.mx@gmail.com>
2023-12-29 21:51:26 -0600

    [UPDATE] 1. Prueba Método OAEP

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,75 @@
-import {Component, OnInit} from '@angular/core';
-import {CryptoService} from "./app/services/crypto.service";
+import { Component, OnInit } from '@angular/core';
+import { CryptoService } from './app/services/crypto.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
   title = 'ng-cipher-lib';
 
-  constructor(private cryptoService: CryptoService) {
+  constructor(
+    private cryptoService: CryptoService,
+  ) {
   }
 
   ngOnInit(): void {
 
     setTimeout(async () => {
-      let value = "NhWuUkvEcBMZS8XdZIm1nln5QoyecExFjk0i6dFV8ainBCfBYtiGPyh1bnL6rXe87WQF2/KKLw68nPaONyULcQLeO/GD1ELgkGF421AVSulQpctfG8Cv8GifcluLT2OcdQe6dx1LBhPL4i0deHuHejcL3gcuccsfLLsgdqlPmBgUM+M416uIDBdaSSiKcG0bYOn1yeSBryT9vu6cww+12A7CLT3wTeHzPwBlvm9C7r15E1zHuJi6uzN4J5j+qO+cgXqIVI+/qNbjbC2McAZSX8ufwDRYuZrnUeylpvuCsBrULKCzrJ0kSmsZriobIhj/nWEr1OI16bdsC7Z7NbYbONyMod4+lp/h44uVfaGk1Xwq7wh7lXyFXL4pz1jbWK8vZiqI1txgL6A2qTsOuPf8RKskO7Ytl59jLs8XJMZRGONWqXvKJ7gMsRaooi0chu5dJLFFimXboryFq/+ZY75Q12lTNiDJvq2R1mxrS8scuB/k0DMayDmJ0o/MApZfpDnmETcOjf84UiRGBfnf5jzlEosWkdIysm904eA1ZbjhWQ++LyA1r5cUXNOzyKcz8bbsyEeGfqBjc7NjK33k0pghlA==";
-      let aesK = "Do3VJxoVc9QBzMpk6/Vhh7xH0pqd+784Sva9BjNR6YY=";
-      const hmacK = "m0sfw6fhuU8vhvJoxZ0r6ZWFZmp26kRh97eihPJntfI=";
+
+      /*let value = 'NhWuUkvEcBMZS8XdZIm1nln5QoyecExFjk0i6dFV8ainBCfBYtiGPyh1bnL6rXe87WQF2/KKLw68nPaONyULcQLeO/GD1ELgkGF421AVSulQpctfG8Cv8GifcluLT2OcdQe6dx1LBhPL4i0deHuHejcL3gcuccsfLLsgdqlPmBgUM+M416uIDBdaSSiKcG0bYOn1yeSBryT9vu6cww+12A7CLT3wTeHzPwBlvm9C7r15E1zHuJi6uzN4J5j+qO+cgXqIVI+/qNbjbC2McAZSX8ufwDRYuZrnUeylpvuCsBrULKCzrJ0kSmsZriobIhj/nWEr1OI16bdsC7Z7NbYbONyMod4+lp/h44uVfaGk1Xwq7wh7lXyFXL4pz1jbWK8vZiqI1txgL6A2qTsOuPf8RKskO7Ytl59jLs8XJMZRGONWqXvKJ7gMsRaooi0chu5dJLFFimXboryFq/+ZY75Q12lTNiDJvq2R1mxrS8scuB/k0DMayDmJ0o/MApZfpDnmETcOjf84UiRGBfnf5jzlEosWkdIysm904eA1ZbjhWQ++LyA1r5cUXNOzyKcz8bbsyEeGfqBjc7NjK33k0pghlA==';
+      let aesK = 'Do3VJxoVc9QBzMpk6/Vhh7xH0pqd+784Sva9BjNR6YY=';
+      let hmacK = 'm0sfw6fhuU8vhvJoxZ0r6ZWFZmp26kRh97eihPJntfI=';
       const dec = await this.cryptoService.decryptAes(value, aesK, hmacK);
       console.log(dec);
 
-      value = "pruebas_pld@yopmail.com";
-      aesK = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA45SIe9RDkgr7vCGPF4dY\nFrF8vZdJv44Puh7AXjaj2OQXUcm8eOFrar0w1rrEZMRB+pO1+QKGubERSgunw5YD\nYPOyG5t8tZepawxnQIXSQU7oEsqzTFShSjs5K6HoLafwwDxi6KD0pGWbOH8NvX3Q\nfjn4P8pHpiAn4ZOflbzG2BeJmzvlwrZRDHqGK/IXiDEsJn1AOob9QIkdTHfHgmU1\n95DjMXxfl4ZQDIV51qaIRHoWa2mXgxiO1GVD+t7/tNOmsQy2rJ3BaZi4p/eDkfX9\ndU1wsQTWn9ZHzmiIZaKu3vdFLg1wwVLlSsKxUuGLSLad2QE4wYtsBBD/OmhdNQi9\njQIDAQAB";
+      value = 'pruebas_pld@yopmail.com';
+      aesK = 'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA45SIe9RDkgr7vCGPF4dY\nFrF8vZdJv44Puh7AXjaj2OQXUcm8eOFrar0w1rrEZMRB+pO1+QKGubERSgunw5YD\nYPOyG5t8tZepawxnQIXSQU7oEsqzTFShSjs5K6HoLafwwDxi6KD0pGWbOH8NvX3Q\nfjn4P8pHpiAn4ZOflbzG2BeJmzvlwrZRDHqGK/IXiDEsJn1AOob9QIkdTHfHgmU1\n95DjMXxfl4ZQDIV51qaIRHoWa2mXgxiO1GVD+t7/tNOmsQy2rJ3BaZi4p/eDkfX9\ndU1wsQTWn9ZHzmiIZaKu3vdFLg1wwVLlSsKxUuGLSLad2QE4wYtsBBD/OmhdNQi9\njQIDAQAB';
       const enc = await this.cryptoService.encryptRSA(value, aesK);
       console.log(enc);
+
+      value = '720000';
+      aesK = 'FA1JItJgBNK/KY0C1qv5S+ZGSVZiAReBLLZf+t46TYY=';
+      hmacK = 'dnqHvZmuRgBk+w5TuRFIhg==';
+      const encAes = await this.cryptoService.encryptAes(value, aesK, hmacK);
+      ; // request entity too large
+      console.log(encAes);*/
+
+
+      // OAE
+      let value = 'miguel.garcias@elektra.com.mx';
+      // let publicKey = 'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA45SIe9RDkgr7vCGPF4dY\nFrF8vZdJv44Puh7AXjaj2OQXUcm8eOFrar0w1rrEZMRB+pO1+QKGubERSgunw5YD\nYPOyG5t8tZepawxnQIXSQU7oEsqzTFShSjs5K6HoLafwwDxi6KD0pGWbOH8NvX3Q\nfjn4P8pHpiAn4ZOflbzG2BeJmzvlwrZRDHqGK/IXiDEsJn1AOob9QIkdTHfHgmU1\n95DjMXxfl4ZQDIV51qaIRHoWa2mXgxiO1GVD+t7/tNOmsQy2rJ3BaZi4p/eDkfX9\ndU1wsQTWn9ZHzmiIZaKu3vdFLg1wwVLlSsKxUuGLSLad2QE4wYtsBBD/OmhdNQi9\njQIDAQAB';
+      // let publicKey = 'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEArHQ09caWERhxf9deHJr2AXwBUQuUdmjuZRRTAvkx+VkytEdycTgt8wNR53jjCCSeyK4EeeU04nzoTW6Sqh77gTK3T9rPy6QPjD7IrFNZeLwwDfy5SFe9G2vzbJ1geyMgSdBAMITOXK9cdAnsCC40dNfPKly3BCf2SI+6Z3zJXghJZfKa2iucGrpW1Jwnxdwbtf7gzYBwmdu7lCz7o93Fpc3Wd7vCCsjl+PafasuGBPPRhqwuo4fbNuTM6D0oaXWd6Y9HoQ6BanortLotoCXOWPCJxqyAU52V1H4lgxP8bgYPZFGRrrabgbqsvDivohSxj4xd822LleF7VZD9GtosUQIDAQAB';
+      let publicKey = `-----BEGIN PUBLIC KEY-----
+MIGeMA0GCSqGSIb3DQEBAQUAA4GMADCBiAKBgGoTDCEi7qSKYxEd7wO+K6g0RqNZ
+t9GBWzU6+U7P2rOsH1o8Jgi/yrA2Ky8yLFcs5cu/9qdLkGHptm5eIDZb06NWyX+6
+vRmJKdbtqsgRP4Ju2gBLBmLBn1I2ewKVuoWG1u1qBkHZSJdboGpj7Ne4ryqf3nJk
+w8p9ugoOoSZDgY9hAgMBAAE=
+-----END PUBLIC KEY-----`;
+      const encryptedValue = await this.cryptoService.encryptRSAOAE(value, publicKey);
+      console.log(encryptedValue);
+
+      // let privateKey = 'MIIEpQIBAAKCAQEArHQ09caWERhxf9deHJr2AXwBUQuUdmjuZRRTAvkx+VkytEdycTgt8wNR53jjCCSeyK4EeeU04nzoTW6Sqh77gTK3T9rPy6QPjD7IrFNZeLwwDfy5SFe9G2vzbJ1geyMgSdBAMITOXK9cdAnsCC40dNfPKly3BCf2SI+6Z3zJXghJZfKa2iucGrpW1Jwnxdwbtf7gzYBwmdu7lCz7o93Fpc3Wd7vCCsjl+PafasuGBPPRhqwuo4fbNuTM6D0oaXWd6Y9HoQ6BanortLotoCXOWPCJxqyAU52V1H4lgxP8bgYPZFGRrrabgbqsvDivohSxj4xd822LleF7VZD9GtosUQIDAQABAoIBAFnrjeHJVl47nJLIPW407qj/e8BChXroY8WYv2ABh6k4dVPDHU0jufHIfWBwjBCm55RECFl2xgiCkK4vRZIm7YUEaMjekxLfzKuX4RLZswFdPsFMUWADP7uYoii0NqZ+Pku+pnmmBeQWG+L1eXkafoeeL4srJNKp86UKLGvhiS3Zz9QgvJKw2nYoe901Jh+nngzhO9MaTGWLgJa04aTMQCZ8qdA1gG6gQ9W9UnVEY7m8CzkSyXFXz/JlvZ/WY8Wl7PM9+2ovl4np5S4ZMaPBJvVm0/W22MsNDMo6fo8/yiGLu72QyY4X8zQVPp5rtPA1tYr9UcEnhi16AHvzDDUaoHECgYEA6sP2fzqYWisJz+d+deZXmcT2CUCscJJAeXmyH+DZreEJFmF2D+SHVx1+dcPSDFjfKgmL3u9l0A4gw4jVz3J64QulZtL/DhNKCj6/NFAjfd2lKx1pcHW3ZOkiMES1FxWPcj7SVkG/PDSetDRQu3mLUwWytch2fwCEGaSLJ6Q45ucCgYEAvA1oq29KCrdcfLH0lh5X58MxHHCGNkT6S2gFY44r8MrQy2Cu3zepQIp+EIVzfVeUALgk7+5gqbkZf+gUKihSIRg0n6CjplTesJQrGfA/IAmPUpSvKSTYUWhxYLgD6JoTNPHcjZIsQKz3objDZmO6zNaaEDiuDo3AhkufBOYFxAcCgYEApAyz30wu0jNo8+MsN27yn/1GgEi1NyS78SbdFfGlqx+pnf4U5h4nhPSTPe/Mg9xGdbWeC/b2CmEU/A5OelvhCgl9V1Dx8gAAqRAXxJKLepS7qlkvKOrZcKaIwXoBoVPrnUXQ7X2cCJIspHg6Qgc5GJbOvsCKLU1LAjA9HUJgkNkCgYEAjunp2A+Vx5seMK3VYZjh2YKf+isR9IBrb484OTa88hxQnZ92hrF4cVJPCCNDZ8h89Cb9+1HAKHxBgtFPMdK+x+Prajzc4fWx36RbNTU95A0f3PSeQbh8pTGoqIZwgf9GBYChyv2BoA1TjJYr+c5lEnMtVpKcxPa5j9Y8na5qR1UCgYEAiA4bqKD7cJy2GfNokZhxjej8Y9QGf8fKL4rhh3aIl4diQgUfi+1F12McPmlwsvfnsuIdGkt/eDdp0Phr/smYbEmKniYQBcHmLc97OH99TyNzySMVeZ5RrWP1WXLkWOeDOzpuZwKrUuXTDvHJfCsEqmlEujUWeGXxOVN/aOFZI2s=';
+      let privateKey = `-----BEGIN RSA PRIVATE KEY-----
+MIICWwIBAAKBgGoTDCEi7qSKYxEd7wO+K6g0RqNZt9GBWzU6+U7P2rOsH1o8Jgi/
+yrA2Ky8yLFcs5cu/9qdLkGHptm5eIDZb06NWyX+6vRmJKdbtqsgRP4Ju2gBLBmLB
+n1I2ewKVuoWG1u1qBkHZSJdboGpj7Ne4ryqf3nJkw8p9ugoOoSZDgY9hAgMBAAEC
+gYAf3hMRzXXWlwDGPrD09egVw4bJQoX3+h3Vsx79L3Lr8bUFnfZrQHt7TvrQVgl+
+My3idYx7nDcHQ3icVcjlq9a74f3rWEvay3HNEbzsXJ2u1sQQt9nXXOg9iOqU9p+q
+AVomE9GNa8hOsODxNWffSxGfDVz/bU0oeFtjSTpzAWlUwQJBAMYpUjfr2RReuprG
+2c7tLrXtpxnGIUIJVwMj3HaG0QGdQk0gZXKNoME1acuX6URZN1CQVzo/G4HgxlmA
+Z0z0gjkCQQCJCPU/bKprjdnFh9xmakIV5zF4yCA6o/PuJ39aazQLrckLWYnLR51h
+OeRWNvB4dWNc4PScWEVi5+WGUtB3QVZpAkEAhCST/lSZaxsMhyApqPLDnnA/aVGV
+ZgsCakakwql+gPwUdYgv/mbxl0X+KdVAqeJwn5nRgTcKKd7xhke5GzlGCQJAYkTH
+Qk1VxL07friX5+WpMHXTd07kNckfAxjV7exUrU9ZjccO1nSrum4+fUj2lxtvPsqe
+/4MZdvzfDr1Se0KiiQJAUPi1OdTw6UlpwNlRlYxY37vUL6jm/ZkdwASphYSFiDvT
+0jixggBKdESb8lXS5MtNzn8b25E2iIPs8k/xER1SMA==
+-----END RSA PRIVATE KEY-----`;
+      const unencryptedValue = await this.cryptoService.decryptRSAOAEP(encryptedValue, privateKey);
+      console.log(unencryptedValue);
+
+      // await this.cryptoService.encrypt();
     });
   }
 
--- src/app/app/services/crypto.service.ts
+++ src/app/app/services/crypto.service.ts
@@ -1,6 +1,9 @@
-import {Injectable} from '@angular/core';
+import { Injectable } from '@angular/core';
 
 import * as crypto from 'crypto';
+
+import * as forge from 'node-forge';
+
 
 @Injectable({
   providedIn: 'root',
@@ -42,7 +45,7 @@ export class CryptoService {
           key,
           padding: crypto.constants.RSA_PKCS1_PADDING,
         },
-        Buffer.from(value)
+        Buffer.from(value),
       );
       return encryptBuff.toString('base64');
     } catch (e) {
@@ -55,6 +58,7 @@ export class CryptoService {
     try {
       const key = '-----BEGIN PRIVATE KEY-----\n' + privateKey + '\n-----END PRIVATE KEY-----';
       const paddingValue = crypto.constants.RSA_PKCS1_PADDING;
+      const paddingValue2 = crypto.constants.RSA_PKCS1_OAEP_PADDING;
 
       // @ts-ignore
       const msg = new Buffer.from(value, 'base64');
@@ -64,6 +68,260 @@ export class CryptoService {
       return '';
     }
   };
+
+  /*var encrypted = publicKey.encrypt(bytes, 'RSA-OAEP', {
+    md: forge.md.sha256.create(),
+    mgf1: {
+      md: forge.md.sha1.create()
+    }
+  });
+
+// decrypt data with a private key using RSAES-OAEP/SHA-256/MGF1-SHA-1
+// compatible with Java's RSA/ECB/OAEPWithSHA-256AndMGF1Padding
+  var decrypted = privateKey.decrypt(encrypted, 'RSA-OAEP', {
+    md: forge.md.sha256.create(),
+    mgf1: {
+      md: forge.md.sha1.create()
+    }
+  }); */
+
+  /*async  demonstrateKeyBasedAsymmetricEncryption()  {
+    try {
+      // replace with yout actual String
+      let exampleString =
+        "Text that is going to be sent over an insecure channel and must be encrypted at all costs!";
+      // generate Keypair, in asynchronous encryption both keys need to be related
+      // and cannot be independently generated keys
+      // keylength adheres to the "ECRYPT-CSA Recommendations" on "www.keylength.com"
+      let keypair = forge.generateKeyPair({ bits: 3072, e: 0x10001 });
+
+      // ENCRYPT String
+      let toEncrypt = Buffer.from(exampleString);
+      let encrypted = forge.util.encode64(
+        keypair.publicKey.encrypt(toEncrypt, "RSA-OAEP")
+      );
+
+      // DECRYPT String
+      let decrypted = keypair.privateKey.decrypt(
+        forge.util.decode64(encrypted),
+        "RSA-OAEP"
+      );
+
+      console.log(
+        "Decrypted String and original String are the same: %s",
+        exampleString.localeCompare(decrypted) === 0 ? "yes" : "no"
+      );
+    } catch (error) {
+      console.log(error.message);
+    }
+  };*/
+
+  /*async encrypt() {
+    const ASYMMETRIC_OPTIONS = {
+      md: forge.md.sha256.create(),
+      mgf: forge.mgf.mgf1.create(forge.md.sha1.create()),
+    };
+    const ASYMMETRIC_OPTIONS2 = {
+      md: forge.md.sha512.create(),
+      mgf1: forge.mgf.mgf1.create(forge.md.sha256.create()),
+    };
+    try {
+      const pubKey = `-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAnkKOsx11+j3xeCO0haPmqK/HdHl9mex3+bdl7whmdztVtgPd5LXVODL+V2hRrURPx83kOnHUUPbFc5QBSQkLSIpoisvCNDyh/RX4tfnMnAS7dUBLE5PQhQQ8LjV/9kosZP/0u61yuCMeH8bmuW8agdClTzNHR7Ok8bdZWvz2P2MzjBNLfmP3RmPp7ego9SFCDaltkw3vqHI9FH8aIqHW1VkvTUQ0FxLUM5GgWkf1fBozSDSmplTzpqQg9vF0QQpSr6+k28htONpKXdHFEMK+rRO8k/+3w01v0dnwoov4qC9ZDhXcvQdKzqFiGXd8eJSn6oQf5LJn5M5VKAxfB8vdWwIDAQAB\n-----END PUBLIC KEY-----`;
+      const publicKey = forge.pki.publicKeyFromPem(pubKey);
+
+      const priKey = '-----BEGIN RSA PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQCFASnCFt+U1ttdDlyRgq55Tdg8y6FFmtZ7CtlSLsnrJBnT5m+8qB8A+culoDUyY0DDmU/ZWvPyuEl0AFQAgnIXMLGOJ0GB3++NxFp1cOJ30MXfQJ7Wwvd1/FwD6M+N8OGq4kjKKgsQEzwO8I8wYsiXUZzNFVzAx9SI/rTIiJ6VSWldfWzTNPecSRf4TTlVwu7B079CnQ/cAB8b5HjKT6X6X2ojx5ixbB8SRMR2TXxd+IAzE5+5SRWaCoXda+vQ9kRbfes9q4sh/tOEnY2d1Ol4eZPv8dGnVNyGYqeOlJSV0MdmClnjzCnuy+RGAbNQsdWN/YT8G5GUj/pe+ER03t4LAgMBAAECggEAWwkNxOwVAnIBWXMwBuS0eRDAFfSmoNa3QSaO0wJ1cD2YdAJArpdACVHwgp/iaWen93ljPvlTCen7VKu401R1tMu+a/cQjk1xUM55v/KQ/8Nk/EblsUdnS1l8vaxHC/c47tbNTZpzUgBoVi1vRIYImBHjSjf58XWVUmNL6AoiyTimdcRAauq0aG0dsA7oE1uONZrRXWHa/YIxu+xOzEiVQrxCfodDO1rcKqjBYfw3DI8309Nykngzk43DkLa0AJshmkFD4C1dbXVQ3HAOPXT9ntr3MB+bnxk4ey1yALMu9v5jKblm5+UHNH6aX7dpxF5VCACU8P/TA6BFZGJ/LvnAoQKBgQC9jDAeY5r6A/d4wuxQMF0bxLuXChDUl23isOcu68TKxTTx0IdNUtI6AzBpxw/Qtea608kTKP4IRLwVmajAx5RVjmz92daiPaV56MrRM2x7Xx105sbCvxGZf4YlOU5arL4H3S6BfzMYec/DpbXcF8SE6yJYlZhBa4g5ZyLoh4uoWwKBgQCzokK4A9Lb9+z19ExHcRYfUbZWwTxHmUG4TLUrpDtHtqk4wvY64lX74NphzXwWi2+NZEtxW9Xi+Kx6a6Q7YTriWJqHVCcyJIUwh+rlSsoplNbigbC33BW43+TrlMrddhpsWS9NHIBfkl2gg1cL6qSoFT6HAHkBk+MRpQINoqkQEQKBgQCNeRvVIrziTKGh1cLxmxdziWQGOLIVKq0OnMEAaWLpgUWiEuVZ1i4otIam/LeWLJ545WfX0CnC3CKR8K1erdfN/87jrXqgf2iJ1DHE8R/P+U8QGMtTQa8OJXtnPx13WiU/01tJtjnZ09k5iu5wxqCcIK/y9Dy+31a+mXcZnUmkrwKBgAZfakiG8jMVj5WGLjKWo3dG+dYIEvBjUn6MUh0TPXsk0soNuJiMMQq3hmfT12QhXDrC5R5HaMYdlljder0UjPc8t3a3mJBh1UvMdpvd1GA3RIxSl4deSnL9lmx5Id1LdFKFs3Tj3vYFBkdMgYkNsPu5jTdS3/gbXAHjPbhoHeFRAoGBAK3IfDccROCa82MZvkugCHCkiYBko2b4pFXKvxBq8LzQEvM+C0YR4zz/UBHR7/CyXZo2CbG0Qy2Ltzeu4qCMrDg7rpxF3lsd+aHSlERMFoPdc4mLNom5oSF1SuLwEFkk6zBA2DO5CLO8lfbIVBt/eFtlrsQKpU86BcmY2q+iAeXq\n-----END RSA PRIVATE KEY-----';
+      const privateKey = forge.pki.privateKeyFromPem(priKey);
+
+      // encrypt data with a public key using RSAES-OAEP/SHA-256/MGF1-SHA-1
+      // compatible with Java's RSA/ECB/OAEPWithSHA-256AndMGF1Padding
+      const encrypted = publicKey.encrypt(
+        'miguel.garcias@elektra.com.mx',
+        'RSA-OAEP',
+        ASYMMETRIC_OPTIONS2,
+      );
+      console.log(window.btoa(encrypted));
+
+      // decrypt data with a private key using RSAES-OAEP/SHA-256/MGF1-SHA-1
+      // compatible with Java's RSA/ECB/OAEPWithSHA-256AndMGF1Padding
+      const decrypted = privateKey.decrypt(
+        window.btoa(encrypted),
+        'RSA-OAEP',
+        ASYMMETRIC_OPTIONS2,
+      );
+      console.log(decrypted);
+    } catch (e) {
+      console.log('encryptRSAOAE: error: ', e);
+      return '';
+    }
+  };*/
+
+  async encryptRSAOAE(
+    value: string,
+    key: string,
+  ) {
+    console.log(key);
+    try {
+      // const normalizedPublicKey = `-----BEGIN PUBLIC KEY----- ${key} -----END PUBLIC KEY-----`;
+      const normalizedPublicKey = this._toKeyWithPem(
+        key,
+        'public',
+      );
+      console.log(normalizedPublicKey);
+
+      const publicKey = forge.pki.publicKeyFromPem(normalizedPublicKey);
+      // const encryptedBytes = publicKey.encrypt(value.toString(), 'RSA-OAEP');
+      // return window.btoa(encryptedBytes);
+      // let toEncrypt = Buffer.from(value);
+      console.log(forge.util.encode64(value));
+
+      // ENCRYPT String
+      /*let toEncrypt = Buffer.from(value);
+      let encrypted = forge.util.encode64(
+        keypair.publicKey.encrypt(toEncrypt, "RSA-OAEP")
+      );*/
+      const messageDigest = this._md5Digest(value).getBytes();
+      console.log(messageDigest);
+      const encrypted = publicKey.encrypt(messageDigest, 'RSA-OAEP', {
+        md: forge.md.sha256.create(),
+        mgf1: {
+          md: forge.md.sha1.create(),
+        },
+      });
+      // console.log(encrypted);
+      return forge.util.encode64(encrypted);
+      // return window.btoa(encrypted);
+
+      // return forge.util.encode64(publicKey.encrypt(forge.util.hexToBytes(value)));
+      // return encrypted;
+    } catch (e) {
+      console.log('encryptRSAOAE: error: ', e);
+      return new Promise<string>((resolve, reject) => {
+        resolve(value);
+      });
+    }
+  };
+
+  _md5Digest(byteString) {
+    const md = forge.md.md5.create();
+    md.update(byteString);
+
+    return md.digest();
+  }
+
+  /*async decryptRSAOAE(value, key) {
+    try {
+      const pKey = '-----BEGIN RSA PRIVATE KEY-----\n' + key + '\n-----END RSA PRIVATE KEY-----';
+      const privateKey = forge.pki.privateKeyFromPem(pKey);
+      const encryptedBytes = privateKey.decrypt(value, 'RSA-OAEP', {
+        md: forge.md.sha256.create(),
+        mgf1: {
+          md: forge.md.sha1.create(),
+        },
+      });
+      console.log(encryptedBytes);
+      return window.btoa(encryptedBytes);
+
+      // return forge.util.encode64(privateKey.decrypt(forge.util.hexToBytes(value)));
+
+      /!*var privateKey = forge.pki.privateKeyFromPem(rsaPrivateKey.val());
+      var ctBytes = forge.util.decode64(value);
+      console.log(ctBytes);
+      var plaintextBytes = privateKey.decrypt(ctBytes);
+      // @ts-ignore
+      return plaintextBytes.toString("utf8");*!/
+    } catch (e) {
+      console.log('decryptRSAOAE: error: ', e);
+      return '';
+    }
+  };*/
+
+
+  // ----
+  public async decryptRSAOAEP(
+    value: string,
+    key: string,
+  ) {
+    try {
+      // const keys = await this.getSecurityKeys();
+      // let prKey = privateKey
+      const normalizedPrivateKey = this._toKeyWithPem(
+        key,
+        'private',
+      );
+      console.log(normalizedPrivateKey);
+
+      const privateKey = forge.pki.privateKeyFromPem(normalizedPrivateKey);
+      var decrypted = privateKey.decrypt(forge.util.decode64(value), 'RSA-OAEP', { // esto es el dato desencriptado
+        md: forge.md.sha256.create(),
+        mgf1: {
+          md: forge.md.sha256.create(),
+        },
+      });
+      // debugger
+      return decrypted;
+    } catch (error) {
+      // debugger
+      console.log(error);
+      return new Promise<string>((resolve, reject) => {
+        resolve(value);
+      });
+    }
+  }
+
+  _toKeyWithPem(key, type) {
+    let localKey = key;
+
+    if (!localKey) {
+      return localKey;
+    }
+
+    if (type === 'public') {
+      localKey =
+        '-----BEGIN PUBLIC KEY-----\n' + key + '\n-----END PUBLIC KEY-----';
+    }
+
+    if (type === 'private') {
+      localKey =
+        '-----BEGIN PRIVATE KEY-----\n' + key + '\n-----END PRIVATE KEY-----';
+    }
+
+    return localKey;
+  }
+
+  /*demonstrateKeyBasedAsymmetricEncryption = () => {
+    try {
+      // replace with yout actual String
+      let exampleString =
+        "Text that is going to be sent over an insecure channel and must be encrypted at all costs!";
+      // generate Keypair, in asynchronous encryption both keys need to be related
+      // and cannot be independently generated keys
+      // keylength adheres to the "ECRYPT-CSA Recommendations" on "www.keylength.com"
+      let keypair = forge.generateKeyPair({ bits: 3072, e: 0x10001 });
+
+      // ENCRYPT String
+      let toEncrypt = Buffer.from(exampleString);
+      let encrypted = forge.util.encode64(
+        keypair.publicKey.encrypt(toEncrypt, "RSA-OAEP")
+      );
+
+      // DECRYPT String
+      let decrypted = keypair.privateKey.decrypt(
+        forge.util.decode64(encrypted),
+        "RSA-OAEP"
+      );
+
+      console.log(
+        "Decrypted String and original String are the same: %s",
+        exampleString.localeCompare(decrypted) === 0 ? "yes" : "no"
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  };*/
+
+
+  // ----
+
 
   async encryptAes(plainText, aesK, hmack) {
     const valueToEncrypt = plainText.replace(/['"]+/g, '');
